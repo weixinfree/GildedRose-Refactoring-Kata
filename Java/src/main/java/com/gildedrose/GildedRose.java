a064c0e7b36d9ea187ae7da38e9bcee416b0855a
wangwei12 <wangwei12@kuaishou.com>
2021-02-01 21:22:01 +0800

    wangwei12: 完善测试用例，覆盖度100%

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,7 +1,11 @@
 package com.gildedrose;
 
 class GildedRose {
-    Item[] items;
+    static final String AGED_BRIE = "Aged Brie";
+    static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+
+    final Item[] items;
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -9,54 +13,67 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+            final Item item = items[i];
+            if (!isAgedBrie(item)
+                    && !isBackStage(item)) {
+                if (item.quality > 0) {
+                    if (!isSulfuras(item)) {
+                        item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (isBackStage(item)) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!isSulfuras(item)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!isAgedBrie(item)) {
+                    if (!isBackStage(item)) {
+                        if (item.quality > 0) {
+                            if (!isSulfuras(item)) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
         }
+    }
+
+    private boolean isSulfuras(Item item) {
+        return item.name.equals(SULFURAS_HAND_OF_RAGNAROS);
+    }
+
+    private boolean isBackStage(Item item) {
+        return item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT);
+    }
+
+    private boolean isAgedBrie(Item item) {
+        return item.name.equals(AGED_BRIE);
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,172 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    public static final int SELL_IN = 10;
+    public static final int QUALITY = 20;
+
+    Item item;
+    GildedRose app;
+
+    @BeforeEach
+    void setup() {
+        item = new Item("demo", SELL_IN, QUALITY);
+        final Item[] items = {item};
+        app = new GildedRose(items);
     }
 
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void test_toString() {
+        assertEquals(item.toString(), "demo, " + SELL_IN + ", " + QUALITY);
+    }
+
+    @Test
+    void test_backstage_item() {
+        // "Backstage passes"（后台通行证）与"Aged Brie"（陈年布利奶酪）类似，
+        // 其品质`Quality`会随着时间推移而提高；
+        item.name = GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT;
+        item.sellIn = 20;
+        for (int i = 0; i < 10; i++) {
+            app.updateQuality();
+            assertEquals(item.sellIn, 20 - i - 1);
+            assertEquals(item.quality, QUALITY + i + 1);
+        }
+    }
+
+    @Test
+    void test_backstage_item_lt_10() {
+        // "Backstage passes"（后台通行证）与"Aged Brie"（陈年布利奶酪）类似，
+        // 当还剩10天或更少的时候，品质`Quality`每天提高2；
+        item.name = GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT;
+        item.sellIn = 20;
+        for (int i = 0; i < 10; i++) {
+            app.updateQuality();
+        }
+        final int quality = item.quality;
+        for (int i = 0; i < 5; i++) {
+            app.updateQuality();
+            assertEquals(item.quality, Math.min(quality + 2 * (i + 1), 50));
+        }
+    }
+
+    @Test
+    void test_backstage_item_lt_5() {
+        // "Backstage passes"（后台通行证）与"Aged Brie"（陈年布利奶酪）类似，
+        // 当还剩5天或更少的时候，品质`Quality`每天提高3；
+        item.name = GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT;
+        item.sellIn = 20;
+        item.quality = 1;
+        for (int i = 0; i < 15; i++) {
+            app.updateQuality();
+        }
+        final int quality = item.quality;
+        for (int i = 0; i < 5; i++) {
+            app.updateQuality();
+            assertEquals(item.quality, Math.min(quality + 3 * (i + 1), 50));
+        }
+    }
+
+    @Test
+    void test_backstage_item_outdated() {
+        // "Backstage passes"（后台通行证）与"Aged Brie"（陈年布利奶酪）类似，
+        // 但一旦过期，品质就会降为0
+        item.name = GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT;
+        item.sellIn = 20;
+        for (int i = 0; i < 20; i++) {
+            app.updateQuality();
+        }
+        app.updateQuality();
+        assertEquals(item.quality, 0);
+    }
+
+    @Test
+    void test_sulfuras_item() {
+        // 传奇物品"Sulfuras"（萨弗拉斯—炎魔拉格纳罗斯之手）永不过期，也不会降低品质`Quality`
+        item.name = GildedRose.SULFURAS_HAND_OF_RAGNAROS;
+        for (int i = 0; i < 100; i++) {
+            app.updateQuality();
+            assertEquals(item.quality, QUALITY);
+            assertEquals(item.sellIn, SELL_IN);
+        }
+    }
+
+    @Test
+    void test_aged_brie_item() {
+        item.name = GildedRose.AGED_BRIE;
+        // "Aged Brie"（陈年布利奶酪）的品质`Quality`会随着时间推移而提高
+        for (int i = 0; i < 10; i++) {
+            app.updateQuality();
+            assertEquals(item.sellIn, SELL_IN - i - 1);
+            assertEquals(item.quality, QUALITY + i + 1);
+            assertTrue(item.quality <= 50);
+        }
+        for (int i = 0; i < 100; i++) {
+            app.updateQuality();
+        }
+        assertEquals(item.quality, 50);
+    }
+
+    @Test
+    void test_aged_brie_bounds() {
+        item.name = GildedRose.AGED_BRIE;
+        for (int i = 0; i < SELL_IN; i++) {
+            app.updateQuality();
+        }
+        final int quality = item.quality;
+        // "Aged Brie"（陈年布利奶酪）的品质`Quality`会随着时间推移而提高
+        for (int i = 0; i < 100; i++) {
+            app.updateQuality();
+            assertEquals(item.sellIn, -(i + 1));
+            assertTrue(item.quality <= 50 && item.quality >= 0);
+            assertEquals(Math.min(quality + 2 * (i + 1), 50), item.quality);
+        }
+    }
+
+    @Test
+    void normal_item() {
+        // 每天结束时，系统会降低每种物品的这两个数值
+        item.name = "normal";
+        for (int i = 0; i < SELL_IN; i++) {
+            app.updateQuality();
+            assertEquals(SELL_IN - i - 1, item.sellIn);
+            assertEquals(QUALITY - i - 1, item.quality);
+        }
+    }
+
+    @Test
+    void normal_item_outdated() {
+        // 一旦销售期限过期，品质`Quality`会以双倍速度加速下降
+        item.name = "normal";
+        for (int i = 0; i < SELL_IN; i++) {
+            app.updateQuality();
+        }
+        int quality = item.quality;
+        for (int i = 0; i < 5; i++) {
+            app.updateQuality();
+            assertTrue(item.sellIn < 0);
+            assertEquals(quality - 2 * (i + 1), item.quality);
+        }
+    }
+
+    @Test
+    void normal_item_bounds() {
+        // 物品的品质`Quality`永远不会为负值
+        // 物品的品质`Quality`永远不会超过50
+        item.name = "normal";
+        for (int i = 0; i < 100; i++) {
+            app.updateQuality();
+            assertTrue(item.quality >= 0 && item.quality <= 50);
+        }
+    }
 }
